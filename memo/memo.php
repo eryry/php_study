0c9fbe5b2487c1378f460663639fc1e5f6226e27
eryry <63179394+eryry@users.noreply.github.com>
2021-02-11 22:17:08 +0900

    空欄入力時処理修正・日付曜日出力追加

--- memo/memo.php
+++ memo/memo.php
@@ -4,46 +4,45 @@ function h($text) {
   $res = htmlspecialchars($text,ENT_QUOTES,'UTF-8');
   return $res;
 }
-
 // データベース使わず、配列に入れて表示させる形のメモ帳
-// 別途データベース使うものも作る
-// これを元にチェックリストにするベース
 
 // クリアボタンで、リストをカラに（リセット）する
 if(!empty($_POST["clear"]) && $_POST["clear"]=="リストクリア") {
   $_SESSION["memolist"]=[];
 }
-
 if(empty($_POST["memo"])) {
-// このままだと、カラでメモするボタン押すと、今までのメモ消えてしまう
-// からの投稿時は、メモ追加しない仕様に修正する
-  $_SESSION["memolist"]= [];
+  // $_SESSION["memolist"][]= "";
+  // これだとカラの値が配列に貼ってしまうので不要
 }
-
 if(!empty($_POST["memo"])) {
   // 値があったら、配列に追加していく
   $_SESSION["memolist"][]= $_POST["memo"];
 }
+// 今日の日付（曜日）
+$today = date("Y年m月d日");
+$week = ["日", "月", "火", "水", "木", "金", "土" ];
+$youbi = $week[date("w")];
 ;?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="utf=8">
   <title>MEMO帳</title>
-  
 </head>
 <body>
   <form action="" method="post">
     <input type="text" name="memo" value="">
     <button>メモする</button>
-
     <input type="submit" value="リストクリア" name="clear">
   </form>
   <?php if($_SESSION["memolist"]): ?>
   <div>
-    <h2>メモリスト</h2>
-    <?php foreach($_SESSION["memolist"] as $memo): ?>
-    <p>□<?php echo h($memo); ?></p>
+    <h2><?=$today."(".$youbi.")"; ?>メモリスト</h2>
+    <?php foreach($_SESSION["memolist"] as $val=>$memo): ?>
+      <?php if($memo !=""): ?>
+        <p><?php echo $val+1; ?>:<?php echo h($memo); ?></p>
+      <?php endif; ?>
     <?php endforeach; ?>
   </div>
   <?php endif; ?>
