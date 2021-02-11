7f2a89bd70005efdce37e5695b460e1da3aeb779
eryry <63179394+eryry@users.noreply.github.com>
2021-02-12 00:00:47 +0900

    ファイル分割・追加と完了更新機能追加

--- todo/todolist001.php
+++ todo/todolist001.php
@@ -1,35 +1,48 @@
 <?php
-// 設計作業
-// ①画面設計書を作る
-// ②テーブル設計
-// ・TODO内容
-// ・TODO完了したかどうか
-// ③
+require_once("todoDatabase.php");
+$res = $pdo->query("SELECT * FROM todos ORDER BY completed,id");
 
-$dsn = 'mysql:dbname=todo;host=localhost';
-$user = "root";
-$password ="root";
+function h($text) {
+  return htmlspecialchars($text,ENT_QUOTES);
+}
 
-$dbh = new PDO($dsn, $user, $password);
-$res = $dbh->query("SELECT * FROM todos ORDER BY completed,id");
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="utf=8">
   <title>TODOリスト</title>
+  <style>
+    ul {
+      list-style:none;
+    }
+    li {
+      display:flex;
+      flex-direction:row;
+    }
+  </style>
 </head>
 <body>
- <h1>TODO</h1>
- <form action="./register_input.php">
- <input type="submit" value="+">
-</form>
-<?php foreach($res as $row): ?>
- <div style="display: flex;">
-  <div styel="flex: 0 0 60px;">☑</div>
-  <div styel="flex: 1 1 0px;"><?php echo $row["contents"]; ?></div>
-  <div styel="flex: 0 0 60px;"><input type="submit" value="ok"></div>
- </div>
- <?php endforeach; ?>
+  <h1>TODO</h1>
+  <form action="classTodo.php" method="post">
+    <input type="text" name="contents" value="">
+    <input type="submit" value="+">
+  </form>
+  <ul>
+  <?php foreach($res as $row): ?>
+  <li>
+    <div><?php echo $row["completed"]==1 ? "☑" : "☐" ;?></div>
+    <div><?php echo h($row["contents"]); ?></div>
+    <?php if($row["completed"]!=1): ?>
+    <div>
+    <form action="classTodo.php" method="post">
+      <input type="hidden" name="id" value="<?php echo $row["id"]; ?>">
+      <input type="submit" value="ok">
+    </form> 
+    </div>
+    <?php endif; ?>
+  </li>
+  <?php endforeach; ?>
+  </ul>
 </body>
 </html>
