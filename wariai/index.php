0d3c03ff7271aaf83eddb99ee6c827ca3e360c5c
eryry <63179394+eryry@users.noreply.github.com>
2021-02-16 23:30:31 +0900

    0除算

--- wariai/index.php
+++ wariai/index.php
@@ -50,35 +50,55 @@ if(!empty($_POST["submit"])) {
     $hoi_done01_hour = calc_hour(intval($_POST["hoi_done01_hour"]));
     $hoi_done01_min  = intval($_POST["hoi_done01_min"]);
     $hoi_done01_alltime = $hoi_done01_hour + $hoi_done01_min;
-    $hoi_done01_wariai = ($hoi_done01_alltime / $hoi_all_time) *100;
+    if($hoi_all_time!=0) {
+      $hoi_done01_wariai = ($hoi_done01_alltime / $hoi_all_time) *100;
+    }else {
+      $hoi_done01_wariai= "-";
+    }
   }
   if(!empty($_POST["hoi_done02"])|| !empty($_POST["hoi_done02_hour"])|| !empty($_POST["hoi_done02_min"])){
     $hoi_done02 = $_POST["hoi_done02"];
     $hoi_done02_hour = calc_hour(intval($_POST["hoi_done02_hour"]));
     $hoi_done02_min  = intval($_POST["hoi_done02_min"]);
     $hoi_done02_alltime = $hoi_done02_hour + $hoi_done02_min;
-    $hoi_done02_wariai = ($hoi_done02_alltime / $hoi_all_time) *100;
+    if($hoi_all_time!=0) {
+      $hoi_done02_wariai = ($hoi_done02_alltime / $hoi_all_time) *100;
+    }else {
+      $hoi_done02_wariai= "-";
+    }
   }
   if(!empty($_POST["hoi_done03"])|| !empty($_POST["hoi_done03_hour"])|| !empty($_POST["hoi_done03_min"])){
     $hoi_done03 = $_POST["hoi_done03"];
     $hoi_done03_hour = calc_hour(intval($_POST["hoi_done03_hour"]));
     $hoi_done03_min  = intval($_POST["hoi_done03_min"]);
     $hoi_done03_alltime = $hoi_done03_hour + $hoi_done03_min;
-    $hoi_done03_wariai = ($hoi_done03_alltime / $hoi_all_time) *100;
+    if($hoi_all_time!=0) {
+      $hoi_done03_wariai = ($hoi_done03_alltime / $hoi_all_time) *100;
+    }else {
+      $hoi_done03_wariai= "-";
+    }
   }
   if(!empty($_POST["hoi_done04"])|| !empty($_POST["hoi_done04_hour"])|| !empty($_POST["hoi_done04_min"])){
     $hoi_done04 = $_POST["hoi_done04"];
     $hoi_done04_hour = calc_hour(intval($_POST["hoi_done04_hour"]));
     $hoi_done04_min  = intval($_POST["hoi_done04_min"]);
     $hoi_done04_alltime = $hoi_done04_hour + $hoi_done04_min;
-    $hoi_done04_wariai = ($hoi_done04_alltime / $hoi_all_time) *100;
+    if($hoi_all_time!=0) {
+      $hoi_done04_wariai = ($hoi_done04_alltime / $hoi_all_time) *100;
+    }else {
+      $hoi_done04_wariai= "-";
+    }
   }
   if(!empty($_POST["hoi_done05"])|| !empty($_POST["hoi_done05_hour"])|| !empty($_POST["hoi_done05_min"])){
     $hoi_done05 = $_POST["hoi_done05"];
     $hoi_done05_hour = calc_hour(intval($_POST["hoi_done05_hour"]));
     $hoi_done05_min  = intval($_POST["hoi_done05_min"]);
     $hoi_done05_alltime = $hoi_done05_hour + $hoi_done05_min;
-    $hoi_done05_wariai = ($hoi_done05_alltime / $hoi_all_time) *100;
+    if($hoi_all_time!=0) {
+      $hoi_done05_wariai = ($hoi_done05_alltime / $hoi_all_time) *100;
+    }else {
+      $hoi_done05_wariai= "-";
+    }
   }
   
   // 婚活事業関連業務時間合計を、計算用に分に変換して変数格納
