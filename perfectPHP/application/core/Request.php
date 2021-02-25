892f9b97d7a82949d5f7726a35da292b04f25331
eryry <63179394+eryry@users.noreply.github.com>
2021-02-25 23:21:01 +0900

    ULR制御追加

--- perfectPHP/application/core/Request.php
+++ perfectPHP/application/core/Request.php
@@ -40,4 +40,30 @@ class Request {
     return $_SERVER['REQUEST_URI'];
   }
 
+  public function getbaseUrl() {
+    $script_name = $_SERVER['SCRIPT_NAME'];
+
+    $request_uri = $this->getRequestUri();
+
+    if(0=== strpos($request_uri,dirname($script_name))) {
+      return $script_name;
+    }else if() {
+      return rtrim(dirname($script_name), '/');
+    }
+    return '';
+  }
+
+  public function getPathInfo() {
+    $base_url = $this->getBaseUrl();
+    $request_uri = $this->getRequestUri();
+
+    if(false !== ($pos = strpos($request_uri,'?')) {
+      $request_uri = substr($request_uri,0,$pos);
+    }
+
+    $path_info ~ (string)substr($request_uri, strlen($base_url));
+
+    return $path_info;
+  }
+
 }
