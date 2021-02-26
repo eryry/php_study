f34b0e32103fed10b63cc80b375935fe88d78cf2
eryry <63179394+eryry@users.noreply.github.com>
2021-02-27 00:07:06 +0900

    計算機能をクラスファイルに移動

---
+++ wariai/ClassCalc.php
@@ -0,0 +1,14 @@
+<?php
+
+class Calc {
+  // 時間を分に変換
+  function calc_hour($hour) {
+    return $hour*60;
+  }
+  // 割合を％で表示
+  function calc_per($time,$all_time) {
+    if($all_time != 0){
+      return ($time / $all_time) *100;
+    }
+  }
+}
--- wariai/calc.php
+++ wariai/calc.php
@@ -1,4 +1,6 @@
 <?php
+require_once 'ClassCalc.php';
+$cal = new Calc();
 
 if(empty($_POST)){
   $all_time_hour = 0;
@@ -33,87 +35,87 @@ if(empty($_POST)){
 
 if(!empty($_POST["submit"])) { 
   // 勤務時間合計を、計算用に分に変換して変数格納
-  $all_time_hour = calc_hour(intval($_POST["all_time_hour"]));
+  $all_time_hour = $cal->calc_hour(intval($_POST["all_time_hour"]));
   $all_time_min  = intval($_POST["all_time_min"]);
   $all_time = $all_time_hour+ $all_time_min;
   
   // 保育事業関連業務時間合計を、計算用に分に変換して変数格納
-  $hoi_all_time_hour = calc_hour(intval($_POST["hoi_all_time_hour"]));
+  $hoi_all_time_hour = $cal->calc_hour(intval($_POST["hoi_all_time_hour"]));
   $hoi_all_time_min  = intval($_POST["hoi_all_time_min"]);
   $hoi_all_time = $hoi_all_time_hour + $hoi_all_time_min;
   // 割合算出
-  $hoi_wariai = ($hoi_all_time / $all_time) *100;
+  $hoi_wariai = $cal->calc_per($hoi_all_time,$all_time);
   
   // 保育事業内訳項目と割合
   if(!empty($_POST["hoi_done01"]) || !empty($_POST["hoi_done01_hour"])|| !empty($_POST["hoi_done01_min"])){
     $hoi_done01 = $_POST["hoi_done01"];
-    $hoi_done01_hour = calc_hour(intval($_POST["hoi_done01_hour"]));
+    $hoi_done01_hour = $cal->calc_hour(intval($_POST["hoi_done01_hour"]));
     $hoi_done01_min  = intval($_POST["hoi_done01_min"]);
     $hoi_done01_alltime = $hoi_done01_hour + $hoi_done01_min;
     if($hoi_all_time!=0) {
-      $hoi_done01_wariai = ($hoi_done01_alltime / $hoi_all_time) *100;
+      $hoi_done01_wariai = $cal->calc_per($hoi_done01_alltime,$hoi_all_time);
     }else {
       $hoi_done01_wariai= "-";
     }
   }
   if(!empty($_POST["hoi_done02"])|| !empty($_POST["hoi_done02_hour"])|| !empty($_POST["hoi_done02_min"])){
     $hoi_done02 = $_POST["hoi_done02"];
-    $hoi_done02_hour = calc_hour(intval($_POST["hoi_done02_hour"]));
+    $hoi_done02_hour = $cal->calc_hour(intval($_POST["hoi_done02_hour"]));
     $hoi_done02_min  = intval($_POST["hoi_done02_min"]);
     $hoi_done02_alltime = $hoi_done02_hour + $hoi_done02_min;
     if($hoi_all_time!=0) {
-      $hoi_done02_wariai = ($hoi_done02_alltime / $hoi_all_time) *100;
+      $hoi_done02_wariai = $cal->calc_per($hoi_done02_alltime,$hoi_all_time);
     }else {
       $hoi_done02_wariai= "-";
     }
   }
   if(!empty($_POST["hoi_done03"])|| !empty($_POST["hoi_done03_hour"])|| !empty($_POST["hoi_done03_min"])){
     $hoi_done03 = $_POST["hoi_done03"];
-    $hoi_done03_hour = calc_hour(intval($_POST["hoi_done03_hour"]));
+    $hoi_done03_hour = $cal->calc_hour(intval($_POST["hoi_done03_hour"]));
     $hoi_done03_min  = intval($_POST["hoi_done03_min"]);
     $hoi_done03_alltime = $hoi_done03_hour + $hoi_done03_min;
     if($hoi_all_time!=0) {
-      $hoi_done03_wariai = ($hoi_done03_alltime / $hoi_all_time) *100;
+      $hoi_done03_wariai = $cal->calc_per($hoi_done03_alltime,$hoi_all_time);
     }else {
       $hoi_done03_wariai= "-";
     }
   }
   if(!empty($_POST["hoi_done04"])|| !empty($_POST["hoi_done04_hour"])|| !empty($_POST["hoi_done04_min"])){
     $hoi_done04 = $_POST["hoi_done04"];
-    $hoi_done04_hour = calc_hour(intval($_POST["hoi_done04_hour"]));
+    $hoi_done04_hour = $cal->calc_hour(intval($_POST["hoi_done04_hour"]));
     $hoi_done04_min  = intval($_POST["hoi_done04_min"]);
     $hoi_done04_alltime = $hoi_done04_hour + $hoi_done04_min;
     if($hoi_all_time!=0) {
-      $hoi_done04_wariai = ($hoi_done04_alltime / $hoi_all_time) *100;
+      $hoi_done04_wariai = $cal->calc_per($hoi_done04_alltime,$hoi_all_time);
     }else {
       $hoi_done04_wariai= "-";
     }
   }
   if(!empty($_POST["hoi_done05"])|| !empty($_POST["hoi_done05_hour"])|| !empty($_POST["hoi_done05_min"])){
     $hoi_done05 = $_POST["hoi_done05"];
-    $hoi_done05_hour = calc_hour(intval($_POST["hoi_done05_hour"]));
+    $hoi_done05_hour = $cal->calc_hour(intval($_POST["hoi_done05_hour"]));
     $hoi_done05_min  = intval($_POST["hoi_done05_min"]);
     $hoi_done05_alltime = $hoi_done05_hour + $hoi_done05_min;
     if($hoi_all_time!=0) {
-      $hoi_done05_wariai = ($hoi_done05_alltime / $hoi_all_time) *100;
+      $hoi_done05_wariai = $cal->calc_per($hoi_done05_alltime, $hoi_all_time);
     }else {
       $hoi_done05_wariai= "-";
     }
   }
   
   // 婚活事業関連業務時間合計を、計算用に分に変換して変数格納
-  $kon_all_time_hour = calc_hour(intval($_POST["kon_all_time_hour"]));
+  $kon_all_time_hour = $cal->calc_hour(intval($_POST["kon_all_time_hour"]));
   $kon_all_time_min  = intval($_POST["kon_all_time_min"]);
   $kon_all_time = $kon_all_time_hour + $kon_all_time_min;
   // 割合算出
-  $kon_wariai = ($kon_all_time / $all_time)*100;
+  $kon_wariai = $cal->calc_per($kon_all_time,$all_time);
   
   // その他業務時間合計を、計算用に分に変換して変数格納
   $else_all_time_hour = intval($_POST["else_all_time_hour"]);
   $else_all_time_min  = intval($_POST["else_all_time_min"]);
   $else_all_time = $else_all_time_hour * 60 + $else_all_time_min;
   // 割合算出
-  $else_wariai = ($else_all_time / $all_time)*100;
+  $else_wariai = $cal->calc_per($else_all_time,$all_time);
 
   
 } else{
@@ -128,14 +130,6 @@ if(!empty($_POST["submit"])) {
   $hoi_dane01_hour=0;
 }
 
-function calc_hour($hour) {
-  return $hour*60;
-}
-function calc_per($time) {
-  if($all_time != 0){
-    return ($time / $all_time) *100;
-  }
-}
 
 
 ?>
