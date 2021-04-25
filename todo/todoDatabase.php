6dee74cf0dad35edcae6d5dd65f4110ac22ea0dd
eryry <63179394+eryry@users.noreply.github.com>
2021-04-25 22:27:14 +0900

    todo,memo,wariaiを、別リポジトリに移動

--- todo/todoDatabase.php
+++
@@ -1,9 +0,0 @@
-<?php
-// ━━━接続情報━━━
-$host="localhost";
-$dbname="todo";
-$dbuser="root";
-$dbpass="";
-// ━━━━━━━━━━
-$dsn = "mysql:host={$host};dbname={$dbname};charset=utf8";
-$pdo = new PDO($dsn, $dbuser, $dbpass);
